d07579bdedebc2e801443d633b4537825d2d1a23
bianchiniriccardo95 <bianchini.riccardo95@gmail.com>
2025-03-01 13:18:28 +0100

    added try

--- R/simulation.R
+++ R/simulation.R
@@ -116,7 +116,7 @@ simulation <- function(n, n_datasets = 1000, seed = 42, treatment_prevalence, tr
         aipw_sl$plot.p_score()
         aipw_sl$plot.ip_weights()
         aipw_weights <- aipw_sl$ip_weights.plot$data$ip_weights
-        bal_tab_aipw <- bal.tab(x = simulated_dataset[,c(1:9)], weights = aipw_weights, s.d.denom = 'pooled', treat = simulated_dataset$treatment, un = T, abs = TRUE, stats = c('mean.diffs','variance.ratios'))
+        bal_tab_aipw <- try(bal.tab(x = simulated_dataset[,c(1:9)], weights = aipw_weights, s.d.denom = 'pooled', treat = simulated_dataset$treatment, un = T, abs = TRUE, stats = c('mean.diffs','variance.ratios')))
         if (inherits(bal_tab_aipw, 'try-error')){
           output <- list(
             smd_results = NA,
